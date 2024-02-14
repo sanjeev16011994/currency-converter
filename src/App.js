2882c3f7bc3dd27c89eaa386d39bdadf51d98abe
sanjeev thakur <sanjeev.thakur@datamoulds.com>
2024-02-14 16:38:52 +0530

    changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,135 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import {
+  Container,
+  Grid,
+  Paper,
+  TextField,
+  MenuItem,
+  Typography,
+  Button,
+} from "@mui/material";
+import CurrencyConverterImage from "./images/exchange.png";
+import { CURRENCY_CODE } from "./currency_code";
 
 function App() {
+  const [amount, setAmount] = useState("");
+  const [fromCurrency, setFromCurrency] = useState("");
+  const [toCurrency, setToCurrency] = useState("");
+  const [convertedAmount, setConvertedAmount] = useState(0);
+  const [loading, setLoading] = useState(false);
+
+  const convertCurrency = () => {
+    const from = fromCurrency.toLowerCase();
+    const to = toCurrency.toLowerCase();
+    const BASE_URL = `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${from}/${to}.json`;
+    setLoading(true);
+    fetch(BASE_URL)
+      .then((response) => response.json())
+      .then((data) => {
+        setConvertedAmount(amount * data[toCurrency.toLowerCase()]);
+      })
+      .catch((error) => console.error("Error fetching exchange rates:", error))
+      .finally(() => {
+        setLoading(false);
+      });
+  };
+
+  const handleAmountChange = (event) => {
+    setAmount(event.target.value);
+  };
+
+  const handleFromCurrencyChange = (event) => {
+    setFromCurrency(event.target.value);
+  };
+
+  const handleToCurrencyChange = (event) => {
+    setToCurrency(event.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container>
+      <Grid
+        container
+        spacing={3}
+        justifyContent="center"
+        style={{ marginTop: "50px" }}
+      >
+        <Grid item xs={12} sm={6}>
+          <Paper elevation={3} style={{ padding: "20px" }}>
+            <div
+              style={{
+                display: "flex",
+                alignItems: "center",
+                marginBottom: "20px",
+                justifyContent:'center'
+              }}
+            >
+              <img
+                src={CurrencyConverterImage}
+                alt="Currency Converter"
+                style={{ marginRight: "10px", height: "50px" }}
+              />
+              <Typography variant="h5" gutterBottom  >
+                Currency Converter
+              </Typography>
+            </div>
+            <TextField
+              label="Amount"
+              type="number"
+              variant="outlined"
+              fullWidth
+              value={amount}
+              onChange={handleAmountChange}
+            />
+            <TextField
+              select
+              label="From"
+              variant="outlined"
+              fullWidth
+              value={fromCurrency}
+              onChange={handleFromCurrencyChange}
+              style={{ marginTop: "20px" }}
+            >
+              {CURRENCY_CODE?.map(({ Currency, Code }) => (
+                <MenuItem key={Code} value={Code}>
+                  {Currency}
+                </MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              select
+              label="To"
+              variant="outlined"
+              fullWidth
+              value={toCurrency}
+              onChange={handleToCurrencyChange}
+              style={{ marginTop: "20px" }}
+            >
+              {CURRENCY_CODE?.map(({ Currency, Code }) => (
+                <MenuItem key={Code} value={Code}>
+                  {Currency}
+                </MenuItem>
+              ))}
+            </TextField>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={convertCurrency}
+              style={{ marginTop: "20px" }}
+              disabled={loading}
+              fullWidth
+            >
+              Convert
+            </Button>
+            <Typography variant="h6" style={{ marginTop: "20px" }}>
+              {convertedAmount !== ""
+                ? `Converted Amount: ${convertedAmount} ${toCurrency}`
+                : "Enter a valid amount"}
+            </Typography>
+          </Paper>
+        </Grid>
+      </Grid>
+    </Container>
   );
 }
 
---
+++ src/currency_code.js
@@ -0,0 +1,70 @@
+export const CURRENCY_CODE =[
+    {
+      "Country": "AFGHANISTAN",
+      "Currency": "Afghani",
+      "Code": "AFN",
+      "Number": 971
+    },
+    {
+      "Country": "AUSTRALIA",
+      "Currency": "Australian Dollar",
+      "Code": "AUD",
+      "Number": 36
+    },
+    {
+      "Country": "BAHAMAS (THE)",
+      "Currency": "Bahamian Dollar",
+      "Code": "BSD",
+      "Number": 44
+    },
+    {
+      "Country": "BAHRAIN",
+      "Currency": "Bahraini Dinar",
+      "Code": "BHD",
+      "Number": 48
+    },
+    {
+      "Country": "BANGLADESH",
+      "Currency": "Taka",
+      "Code": "BDT",
+      "Number": 50
+    },
+    {
+      "Country": "BHUTAN",
+      "Currency": "Ngultrum",
+      "Code": "BTN",
+      "Number": 64
+    },
+    {
+      "Country": "FRANCE",
+      "Currency": "Euro",
+      "Code": "EUR",
+      "Number": 978
+    },
+    {
+      "Country": "HUNGARY",
+      "Currency": "Forint",
+      "Code": "HUF",
+      "Number": 348
+    },
+    {
+      "Country": "ICELAND",
+      "Currency": "Iceland Krona",
+      "Code": "ISK",
+      "Number": 352
+    },
+    {
+      "Country": "INDIA",
+      "Currency": "Indian Rupee",
+      "Code": "INR",
+      "Number": 356
+    },
+    {
+      "Country": "UNITED STATES OF AMERICA",
+      "Currency": "US Dollar",
+      "Code": "USD",
+      "Number": 840
+    }
+]
+
+  
